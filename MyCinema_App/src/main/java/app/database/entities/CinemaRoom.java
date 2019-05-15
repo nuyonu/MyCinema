6a50bc325d7482b0d1265fb2c543657d645616c4
nuyonu <nuyonu@gmail.com>
2019-05-15 22:51:03 +0300

    Câteva modificări
    -Refăcut unele bug-uri din Movies
    -Refăcut unele bug-uri din Rooms
    -Search-ul din Movies aduce cu tot cu imagini filmele
    -Adăugat pagina pentru Program (cea pentru utilizatori) @pmaxim98
    -Realizată conexiunea cu Program din pagina Movies (Book now) @pmaxim98
    
    Co-Authored-By: Paul Maxim <pmaxim98@users.noreply.github.com>

--- MyCinema_App/src/main/java/app/database/entities/CinemaRoom.java
+++ MyCinema_App/src/main/java/app/database/entities/CinemaRoom.java
@@ -13,9 +13,14 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.stream.Stream;
 
 @EntityScan
 @Getter
@@ -41,5 +46,15 @@ public class CinemaRoom implements Serializable
     public CinemaRoom(String name)
     {
         this.name = name;
+    }
+
+    public long getNumberOfImages() {
+        long count = 0;
+        try (Stream<Path> files = Files.list(Paths.get("src/main/resources/static/images/cinema-rooms/" + id))) {
+            count = files.count();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+        return count;
     }
 }
